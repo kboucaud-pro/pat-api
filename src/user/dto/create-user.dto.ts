bca05099c740ddbfbf20d213a5ece7e1d7594b4e
kboucaud <kboucaud.pro@gmail.com>
2024-04-17 20:02:36 +0200

    setup database | create user entity

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,30 @@
+import { IsAlphanumeric, IsEmail, IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
+
+const passwordRegEx =
+	/^(?=.[a-z])(?=.[A-Z])(?=.d)(?=.[@$!%?&])[A-Za-zd@$!%?&]{8,20}$/;
+
+export class CreateUserDto {
+	@IsString()
+	@MinLength(2, { message: 'Name must have at least 2 characters' })
+	@IsNotEmpty()
+	name: string;
+
+	@IsString()
+	@MinLength(3, { message: 'Username must have at least 3 characters' })
+	@IsAlphanumeric(null, { message: 'Username must only be composed of alphanumeric characters' })
+	username: string;
+
+	@IsNotEmpty()
+	@IsEmail(null, { message: 'Please provide a valid email' })
+	email: string;
+
+	@IsNotEmpty()
+	@Matches(passwordRegEx, {
+		message: `Password must contain Minimum 8 and maximum 20 characters,
+		at least one uppercase letter,
+		one lowercase letter,
+		one number and 
+    one special character`,
+	})
+	password: string;
+}
